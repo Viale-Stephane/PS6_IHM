b1496f156dd5eadcedfef3797135db03d87747a3
Viale-Stephane <34646444+Viale-Stephane@users.noreply.github.com>
2019-04-14 22:48:23 +0200

    added some components which will useful later when we will implements profile and research

--- GeoResto/app/src/main/java/com/example/georesto/RegisterActivity.java
+++ GeoResto/app/src/main/java/com/example/georesto/RegisterActivity.java
@@ -1,6 +1,12 @@
 package com.example.georesto;
 
 import android.app.Activity;
+import android.os.Bundle;
 
 public class RegisterActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.register);
+    }
 }
