d638f2c31bbbcbd1a4a469fcf153249a2541161a
Viale-Stephane <34646444+Viale-Stephane@users.noreply.github.com>
2019-05-07 21:46:21 +0200

    landscape for login

--- GeoResto/app/src/main/java/com/example/georesto/Activity/LoginActivity.java
+++ GeoResto/app/src/main/java/com/example/georesto/Activity/LoginActivity.java
@@ -2,6 +2,7 @@ package com.example.georesto.Activity;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -33,6 +34,18 @@ public class LoginActivity extends Activity { // sans oublier l'implémentation 
         final EditText password = findViewById(R.id.password);
         final TextView wrongIdentifiers = findViewById(R.id.wrongIdentifiers);
 
+
+        if(getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE){
+            System.out.println(connect.getX());
+            connect.setX(-400);
+            register.setX(400);
+            register.setY(-175);
+            passwordForgotten.setY(-175);
+            System.out.println(connect.getY());
+            System.out.println(register.getX());
+            System.out.println(register.getY());
+        }
+
         connect.setOnClickListener(new View.OnClickListener() {
             public void onClick(View connect) {
                 if (model.connect(username.getText().toString(), password.getText().toString()) != null) {
@@ -43,6 +56,7 @@ public class LoginActivity extends Activity { // sans oublier l'implémentation 
             }
 
         });
+
 
         register.setOnClickListener(new View.OnClickListener() {
             public void onClick(View passwordForgotten) {
